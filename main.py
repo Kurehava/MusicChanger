ee68f0ffec990e8c0fabcde970dbf52a5dafad87
Kureha_q <orikiringi@gmail.com>
2022-09-21 19:07:43 +0900

    import pycharm

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from pydub import AudioSegment
 if __name__=="__main__":
     # 到时候换成Win32 API
     # https://blog.csdn.net/weixin_44634704/article/details/123006497
-    kugou_floder_path = input("KuGou Music Download Paht >> ")
+    kugou_folder_path = input("KuGou Music Download Paht >> ")
     
