4777efc3d777ca86d216571721960f6f8c7e5657
Kureha_q <orikiringi@gmail.com>
2022-09-21 19:04:20 +0900

    Changes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from pydub import AudioSegment
 
 if __name__=="__main__":
+    # 到时候换成Win32 API
+    # https://blog.csdn.net/weixin_44634704/article/details/123006497
+    kugou_floder_path = input("KuGou Music Download Paht >> ")
     
